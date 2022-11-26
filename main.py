c56640ed29c060dc62fcd9ff9249acb0798d96f2
Taipei Native <85215798+taipeinative@users.noreply.github.com>
2022-11-26 10:07:46 +0800

    alpha 0.1.2 Regular Update
    Update: `readfile.py`, `main.py`

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import readfile as rf                                   # Import readfile module; require $ pip install pandas, openpyxl
 import restaurants as rs                                # Import restaurants module
-import pandas as pd
 
 def main():                                             # Main workflow
     sheet = rf.Sheet()
-    sheet.set_df(pd.DataFrame({
-            'Type': ['Rice','Noodle','Rice'],
-            'Name': ['TR Bento Taipei Branch', 'Toscanini Pasta', 'Hi Sushi']
-        }))
-    print(sheet.query(['s'],False,['Name']))
+    print(sheet.query(kw = ['*'], strict = True , column = ['Ok']))
 
 if __name__ == '__main__': main()
--- readfile.py
+++ readfile.py
@@ -1,5 +1,6 @@
 import pandas as pd                                                                     # Import pandas module; require openpyxl module
-import restaurants as rs                                                                # Import restaurants module.
+import time                                                                             # Import time module.
+import re
 
 class Sheet():
     '''
@@ -12,6 +13,8 @@ class Sheet():
     
     Methods
     ----------
+    query()
+        Query objects by conditions.
     set_`<property>`()
         Set the given property of the object; for example, set_name().
     '''
@@ -44,26 +47,6 @@ class Sheet():
     ### Methods ###
 
     # Following are other methods can be called in Sheets.
-
-    def at(self, row: int, column: int):
-        '''
-        Access data from an integer row/column pair.
-
-        Parameters
-        --------
-        row : int
-            The row index of the cell.
-        column : int
-            The column index of the cell.
-
-        Returns
-        --------
-        df : pd.DataFrame
-            The given cell's dataframe.
-        '''
-        df = self.df.iat[row,column]
-        return df
-
 
     def query(self, kw: list , strict: bool = True , column: list = False):
         '''
@@ -127,24 +110,54 @@ class Sheet():
 
             Examples
             --------
-            >>> 
+            (This function is a local function)
+            >>> df = rf.Sheet()
+            >>> df.set_df(pd.DataFrame({
+                'Type': ['Rice','Noodle','Rice'],
+                'Name': ['TR Bento Taipei Branch', 'Toscanini Pasta', 'Hi Sushi']
+            }))
+            >>> list_kw = ['a','b','c']
+            >>> for i in range(len(list_kw)):
+                    lenient_query(list_kw[i])
+                 Type                    Name
+            0    Rice  TR Bento Taipei Branch
+            1  Noodle         Toscanini Pasta
+               Type                    Name
+            0  Rice  TR Bento Taipei Branch
+                 Type                    Name
+            0    Rice  TR Bento Taipei Branch
+            1  Noodle         Toscanini Pasta
+            2    Rice                Hi Sushi
+            
+            Query only in specific columns:
+            >>> for i in range(len(list_kw)):
+                    lenient_query(list_kw[i],['Name'])
+                 Type                    Name
+            0    Rice  TR Bento Taipei Branch
+            1  Noodle         Toscanini Pasta
+            (None)
+                 Type                    Name
+            0    Rice  TR Bento Taipei Branch
+            1  Noodle         Toscanini Pasta
             '''
 
-            temporary_df_lenient = pd.DataFrame(columns = self.df.columns.values)
+            temporary_df_lenient = pd.DataFrame(columns = self.df.columns.values) # Create an empty dataframe based on callee's columns
 
-            if isinstance(column_names, list):
+            if isinstance(column_names, list): # Test whether `column_name` is a list
 
-                for j in range(len(column_names)):
+                for j in range(len(column_names)): # Loop `len(column_name)` times
+
+                    if column_names[j] in self.df.columns: # Test whether `column_names[j]` is a valid column name
                     
-                    temporary_df_lenient = pd.concat([temporary_df_lenient, self.df[self.df[column_names[j]].str.contains(item)]])
+                        temporary_df_lenient = pd.concat([temporary_df_lenient, self.df.loc[self.df[column_names[j]].str.contains(re.escape(item))]]) # Concatenate the search result dataframes, based on lenient searching and `column_names`
 
-                result = temporary_df_lenient.dropna(subset=['Name']).sort_index()
+                result = temporary_df_lenient.dropna(how = 'all').sort_index() # Drop the first row (the initial `temporary_df_lenient`)
 
                 return result
 
             else:
 
-                return self.df[self.df.apply(lambda row: row.astype(str).str.contains(item, case = False).any(), axis=1)]
+                return self.df[self.df.apply(lambda row: row.astype(str).str.contains(item, case = False).any(), axis=1)] # Return the search result dataframes, based on lenient searching
 
 
 
@@ -167,41 +180,91 @@ class Sheet():
 
             Examples
             --------
-            >>> 
+            (This function is a local function)
+            >>> df = rf.Sheet()
+            >>> df.set_df(pd.DataFrame({
+                'Type': ['Rice','Noodle','Rice'],
+                'Name': ['TR Bento Taipei Branch', 'Toscanini Pasta', 'Hi Sushi']
+            }))
+            >>> list_kw = ['Rice','Noodle']
+            >>> for i in range(len(list_kw)):
+                    strict_query(list_kw[i])
+               Type                    Name
+            0  Rice  TR Bento Taipei Branch
+            2  Rice                Hi Sushi
+                 Type             Name
+            1  Noodle  Toscanini Pasta
+
+            Query only in specific columns:
+            >>> strict_query(['Hi Sushi'])
+               Type      Name
+            2  Rice  Hi Sushi
             '''
 
-            temporary_df_strict = pd.DataFrame(columns = self.df.columns.values)
+            temporary_df_strict = pd.DataFrame(columns = self.df.columns.values) # Create an empty dataframe based on callee's columns
 
-            if isinstance(column_names,list):
+            if isinstance(column_names,list): # Test whether `column_name` is a list
 
-                for j in range(len(column_names)):
-                    
-                    temporary_df_strict = pd.concat([temporary_df_strict, self.df[self.df[column_names[j]] == item]])
+                for j in range(len(column_names)): # Loop `len(column_name)` times
 
-                result = temporary_df_strict.dropna(subset=['Name']).sort_index()
+                    if column_names[j] in self.df.columns: # Test whether `column_names[j]` is a valid column name
+
+                        temporary_df_strict = pd.concat([temporary_df_strict, self.df[self.df[column_names[j]] == item]]) # Concatenate the search result dataframes, based on strict searching and `column_names`
+
+                result = temporary_df_strict.dropna(how = 'all').sort_index() # Drop the first row (the initial `temporary_df_strict`)
 
                 return result
 
             else:
 
-                return self.df[self.df.isin([item]).any(axis = 1)]
+                return self.df[self.df.isin([item]).any(axis = 1)] # Return the search result dataframes, based on strict searching
 
 
 
 
         result = []
-        temporary_df = pd.DataFrame(columns = self.df.columns.values)
+        temporary_df = pd.DataFrame(columns = self.df.columns.values) # Create an empty dataframe based on callee's columns
 
-        for i in range(len(kw)):
+        for i in range(len(kw)): # Loop `len(kw)` times
 
-            if strict == True:
+            if strict == True: # Test whether `strict` is a True (strict mode enabled?)
 
-                temporary_df = pd.concat([temporary_df, strict_query(kw[i],column)]).drop_duplicates()
+                temporary_df = pd.concat([temporary_df, strict_query(kw[i],column)]).drop_duplicates() # Concatenate the search result dataframes, using `strict_query()`
 
             else:
                 
-                temporary_df = pd.concat([temporary_df, lenient_query(kw[i],column)]).drop_duplicates()
+                temporary_df = pd.concat([temporary_df, lenient_query(kw[i],column)]).drop_duplicates() # Concatenate the search result dataframes, using `lenient_query()`
         
-        result = temporary_df.dropna(subset=['Name']).sort_index()
+        result = temporary_df.dropna(how = 'all').sort_index() # Drop the first row (the initial `temporary_df`)
 
-        return result
+        if result.empty != True: # Test whether result is an empty dataframe
+
+            return result
+
+        else: 
+
+            return None # Return `None` if the dataframe is empty
+
+
+def timestamp(msg):
+    '''
+    Prints a message with current local time.
+
+    Parameters
+    --------
+    msg : any
+        The message to be print.
+
+    Returns
+    --------
+    None
+        Returns none; the function directly prints message in console.
+
+    Examples
+    --------
+    >>> timestamp('Hello world')
+    [00:00:00] Hello world
+
+    (The time differs based on current time; it use HH:MM:SS)
+    '''
+    print (f'{time.strftime( "[%H:%M:%S]", time.localtime(time.time()))} {msg}') # Prints message.
